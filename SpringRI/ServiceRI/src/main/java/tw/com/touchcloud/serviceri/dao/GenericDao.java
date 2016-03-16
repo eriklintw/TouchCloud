d22b9b83fc1197cb56139b36acb9db554dfafda4
eriklintw <eriklintw@gmail.com>
2016-03-16 11:54:44 +0800

    Maven Pom Project add ServiceRI

---
+++ SpringRI/ServiceRI/src/main/java/tw/com/touchcloud/serviceri/dao/GenericDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.com.touchcloud.serviceri.dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author ErikLin
+ */
+public interface GenericDao <E,K> {
+    public void add(E entity) ;
+    public void saveOrUpdate(E entity) ;
+    public void update(E entity) ;
+    public void remove(E entity);
+    public E find(K key);
+    public List<E> getAll() ;
+}
---
+++ SpringRI/ServiceRI/src/main/java/tw/com/touchcloud/serviceri/dao/PowerStatusDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tw.com.touchcloud.serviceri.dao;
+
+import tw.com.touchcloud.serviceri.entity.PowerStatus;
+
+
+/**
+ *
+ * @author ErikLin
+ */
+public interface PowerStatusDao extends GenericDao<PowerStatus,Long>{
+    
+}
